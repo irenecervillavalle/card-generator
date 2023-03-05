fcbc9338eab418237e7cfe0376cad76398269fd1
irenecervillavalle <irenecervillavalle@gmail.com>
2023-03-05 08:01:46 +0000

    added task

--- src/app.js
+++ src/app.js
@@ -2,10 +2,43 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function aleatorio(datos) {
+    return datos[Math.floor(Math.random() * datos.length)];
+  }
+
+  let suits = ["♦", "♥", "♠", "♣"];
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "Q"
+  ];
+  let card = document.querySelector(".card");
+  let cardSuit1 = [aleatorio(suits)];
+  console.log(cardSuit1);
+  let result1 = document.querySelector("#top");
+  result1.innerHTML = cardSuit1;
+
+  let result2 = document.querySelector("#bottom");
+  result2.innerHTML = cardSuit1;
+
+  let numRandom = [aleatorio(numbers)];
+  console.log(numRandom);
+  let result3 = document.querySelector(".js-number");
+  result3.innerHTML = numRandom;
+  if (cardSuit1 == "♥" || cardSuit1 == "♦") {
+    card.classList.add("red");
+  } else {
+    card.classList.add("black");
+  }
 };
